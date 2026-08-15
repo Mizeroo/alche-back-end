3eed9033cfc976def56dc11ab7132c99a35f22e5
Mizeroo <l.mizero@alustudent.com>
2026-08-15 15:08:45 +0400

    Update 1-export_to_CSV.py

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """Module that exports an employee's TODO list data to CSV format."""
 import csv
-import requests 
-import sys 
+import requests
+import sys
 if __name__ == "__main__":
     employee_id = int(sys.argv[1])
     BASE_URL = 'https://jsonplaceholder.typicode.com'
