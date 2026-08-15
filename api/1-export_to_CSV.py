6c54c7fab867d2561ffec3095c48ce30b7ebf4b8
Mizeroo <l.mizero@alustudent.com>
2026-08-15 11:02:44 +0000

    Add scv file

---
+++ api/1-export_to_CSV.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Module that exports an employee's TODO list data to CSV format."""
+import csv
+import sys
+
+import requests
+
+
+if __name__ == "__main__":
+    employee_id = int(sys.argv[1])
+    BASE_URL = 'https://jsonplaceholder.typicode.com'
+
+    r = requests.get("{}/users/{}".format(BASE_URL, employee_id))
+    data = r.json()
+    username = data["username"]
+
+    todos_r = requests.get(
+        "{}/todos".format(BASE_URL),
+        params={"userId": employee_id}
+    )
+    todos_data = todos_r.json()
+
+    filename = "{}.csv".format(employee_id)
+    with open(filename, "w", newline="") as csvfile:
+        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        for task in todos_data:
+            writer.writerow([
+                employee_id,
+                username,
+                task["completed"],
+                task["title"]
+            ])
